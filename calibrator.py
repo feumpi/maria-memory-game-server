01c68cdd18d92b5f4ad0fa37e7716b86fd030df6
Felipe Umpierre <felipe.umpierre@edu.ufes.br>
2021-11-22 11:31:03 -0300

    create server logic using added modules

--- calibrator.py
+++ calibrator.py
@@ -5,29 +5,30 @@ from camera import Camera
 
 class Calibrator:
 
-    def __init__(self):
+    def __init__(self, threshold1=150, threshold2=200):
 
-        print("init")
+        self.threshold1 = threshold1
+        self.threshold2 = threshold2
 
-    def filter_image(self, img):
+    def find_contours(self, img):
         img_gray = Camera.to_gray(img)
 
         # Get thresholds from the sliders and apply to create final image
-        threshold1, threshold2 = cv2.getTrackbarPos(
-            "Threshold1", "Parameters"), cv2.getTrackbarPos("Threshold2", "Parameters")
+        #threshold1, threshold2 = cv2.getTrackbarPos(
+        #    "Threshold1", "Parameters"), cv2.getTrackbarPos("Threshold2", "Parameters")
 
-        img_canny = cv2.Canny(img_gray, threshold1, threshold2)
+        img_canny = cv2.Canny(img_gray, self.threshold1, self.threshold2)
 
         # Dilate the image
         kernel = np.ones((5, 5))
         img_dil = cv2.dilate(img_canny, kernel, iterations=1)
 
-        return img_dil
-
-    def draw_shapes(self, output, source):
-
         contours, hierarchy = cv2.findContours(
-            source, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            img_dil, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        return contours, img_dil
+
+    def draw_shapes(self, img, contours):
 
         for contour in contours:
 
@@ -42,9 +43,9 @@ class Calibrator:
             if area > 5000 and area < 50000:
 
                 #cv2.drawContours(img_out, contours, -1, (255, 0, 255), 7)
-                cv2.rectangle(output, (x1, y1), (x2, y2), (0, 255, 0), 5)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
-        return output
+        return img
 
 
 def main():
@@ -56,16 +57,18 @@ def main():
     # Create window for Threshold bars
     cv2.namedWindow("Parameters")
     cv2.resizeWindow("Parameters", 640, 240)
-    cv2.createTrackbar("Threshold1", "Parameters", 150, 255, lambda: None)
-    cv2.createTrackbar("Threshold2", "Parameters", 200, 255, lambda: None)
+    cv2.createTrackbar("Threshold1", "Parameters",
+                       calibrator.threshold1, 255, lambda: None)
+    cv2.createTrackbar("Threshold2", "Parameters",
+                       calibrator.threshold2, 255, lambda: None)
 
     while True:
 
         img = camera.get_frame()
 
-        filtered_img = calibrator.filter_image(img)
+        contours, filtered_img = calibrator.find_contours(img)
 
-        img = calibrator.draw_shapes(img, filtered_img)
+        img = calibrator.draw_shapes(img, contours)
 
         camera.show_image(filtered_img, "Filtered image")
         camera.show_image(img)
--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-print("MT21 Memory Game Server")
+import keyboard
+from calibrator import Calibrator
+from camera import Camera
+from hand_tracker import HandTracker
+from udp_socket import UdpSocket
+
+
+camera = Camera()
+hand_tracker = HandTracker()
+calibrator = Calibrator()
+
+udp_socket = UdpSocket(udp_ip="127.0.0.1", send_port=8000, receive_port=8001,
+                       enable_receive=True, suppress_warnings=False)
+
+contours = []
+
+
+def calibrate():
+    print("Calibrating...")
+    img = camera.get_frame()
+    cont, filtered_img = calibrator.find_contours(img)
+    return cont
+
+
+# Initial calibration
+contours = calibrate()
+
+while True:
+
+    # Recalibrate, if requested with 'c' key press
+    if keyboard.is_pressed('c'):
+        print("Recalibrating...")
+        contours = calibrate()
+
+    # Try to read data from the game
+    data = udp_socket.read_received_data()
+
+    if data != None:
+        print(data)
+        udp_socket.send_data("Sent from server")
+
+    img = camera.get_frame()
+
+    hand_landmarks = hand_tracker.find_hand(img)
+
+    if hand_landmarks:
+        img = hand_tracker.draw_hand(img, hand_landmarks)
+
+    # Draw previously found contours
+    img = calibrator.draw_shapes(img, contours)
+
+    camera.show_image(img)
